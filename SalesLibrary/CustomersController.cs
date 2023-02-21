264ef80d75e0743ed9dfba79bbac406aa6ec5c2a
Trevor Shepherd <t.rayshpd@gmail.com>
2023-02-21 14:51:14 -0500

    Add project files.

---
+++ SalesLibrary/CustomersController.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesLibrary
+{
+    public class CustomersController
+    {
+
+        private SqlConnection sqlConnection { get; set; }
+
+        public List<Customer> GetBySalesRange(decimal low, decimal high)
+        {
+            string sqlStatement = "SELECT * From Customers " +
+                                  $" Where Sales >= {low} and Sales <= {high};";
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            List<Customer> customers = new List<Customer>();
+            while (sqlReader.Read())
+            {
+                Customer customer = new Customer();
+                customer.Id = Convert.ToInt32(sqlReader["Id"]);
+                customer.Name = Convert.ToString(sqlReader["Name"]);
+                customer.City = Convert.ToString(sqlReader["City"]);
+                customer.State = Convert.ToString(sqlReader["State"]);
+                customer.Sales = Convert.ToDecimal(sqlReader["Sales"]);
+                customer.Active = Convert.ToBoolean(sqlReader["Active"]);
+                customers.Add(customer);
+            }
+            sqlReader.Close();
+            return customers;
+        }
+
+        public List<Customer> GetAll()
+        {
+            string sqlStatement = "SELECT * From Customers;";
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            List<Customer> customers = new List<Customer>();
+            while(sqlReader.Read())
+            {
+                Customer customer = new Customer();
+                customer.Id = Convert.ToInt32(sqlReader["Id"]);
+                customer.Name = Convert.ToString(sqlReader["Name"]);
+                customer.City = Convert.ToString(sqlReader["City"]);
+                customer.State = Convert.ToString(sqlReader["State"]);
+                customer.Sales = Convert.ToDecimal(sqlReader["Sales"]);
+                customer.Active = Convert.ToBoolean(sqlReader["Active"]);
+                customers.Add(customer);
+            }
+            sqlReader.Close();
+            return customers;
+        }
+
+        public Customer? GetById(int Id)
+        {
+            string sqlStatement = $"SELECT * From Customers Where Id = {Id};";
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            SqlDataReader sqlReader = sqlCommand.ExecuteReader();
+            if (!sqlReader.HasRows)
+            {
+                sqlReader.Close();
+                return null;
+            }
+            sqlReader.Read();
+            Customer customer = new Customer();
+            customer.Id = Convert.ToInt32(sqlReader["Id"]);
+            customer.Name = Convert.ToString(sqlReader["Name"]);
+            customer.City = Convert.ToString(sqlReader["City"]);
+            customer.State = Convert.ToString(sqlReader["State"]);
+            customer.Sales = Convert.ToDecimal(sqlReader["Sales"]);
+            customer.Active = Convert.ToBoolean(sqlReader["Active"]);
+            sqlReader.Close();
+            return customer;
+        }
+
+        public bool AddCustomer(Customer customerInstance)
+        {
+            string sqlStatement = "INSERT Customers (Name, City, State, Sales, Active) VALUES " +
+                                  $" ( '{customerInstance.Name}', '{customerInstance.City}', '{customerInstance.State}', {customerInstance.Sales}, {(customerInstance.Active ? 1 : 0)}); ";
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            int rowsAffected = sqlCommand.ExecuteNonQuery();
+            if (rowsAffected == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool UpdateRow(Customer customerInstance)
+        {
+            string sqlStatement = " UPDATE Customers SET " +
+                                  $" Name = '{customerInstance.Name}', " +
+                                  $" City = '{customerInstance.City}', " +
+                                  $" State = '{customerInstance.State}', " +
+                                  $" Sales = '{customerInstance.Sales}', " +
+                                  $" Active = {(customerInstance.Active ? 1 : 0)} " +
+                                  $" Where Id = {customerInstance.Id}; ";
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            int rowsAffected = sqlCommand.ExecuteNonQuery();
+            if (rowsAffected == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool DeleteRow(int Id)
+        {
+            string sqlStatement = " DELETE Customers " +
+                                  $" Where Id = {Id}; ";                               
+            SqlCommand sqlCommand = new SqlCommand(sqlStatement, sqlConnection);
+            int rowsAffected = sqlCommand.ExecuteNonQuery();
+            if (rowsAffected == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public CustomersController(string server, string instance) 
+        {
+            string connectionString = $"server={server}\\{instance};" +
+                                      "database=SalesDb;" +
+                                      "trusted_connection=true;" +
+                                      "trustServerCertificate=true;";
+            sqlConnection = new SqlConnection(connectionString);
+            sqlConnection.Open();
+            if (sqlConnection.State != System.Data.ConnectionState.Open)
+            {
+                throw new Exception("Connection failed!");
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if(sqlConnection.State == System.Data.ConnectionState.Open)
+            {
+                sqlConnection.Close();
+            }
+        }
+
+    }
+}
---
+++ SqlSalesLibrary/Program.cs
@@ -0,0 +1,67 @@
+﻿using SalesLibrary;
+
+CustomersController custCtrl = new CustomersController("localhost", "sqlexpress");
+
+List<Customer> customers = custCtrl.GetBySalesRange(20000, 50000);
+
+foreach(Customer eachCustomer in customers)
+{
+    Console.WriteLine($"{eachCustomer.Id} | {eachCustomer.Name} | {eachCustomer.City}, {eachCustomer.State} | {eachCustomer.Sales} | {eachCustomer.Active} ");
+}
+
+//Customer max = custCtrl.GetById(36);
+
+//if(max is null)
+//{
+//    Console.WriteLine("Not found");
+//    return;
+//}
+
+//max.Name = "MAX Technical Training";
+
+//bool success = custCtrl.UpdateRow(max);
+
+//if(!success)
+//{
+//    Console.WriteLine("Update failed!");
+//    return;
+//}
+
+//Customer maxCustomer = new Customer
+//{
+//    Id = 0,
+//    Name = "MAX",
+//    City = "Mason",
+//    State = "OH",
+//    Sales = 1000,
+//    Active = true
+//};
+
+//bool success = custCtrl.AddCustomer(maxCustomer);
+
+//if(!success)
+//{
+//    Console.WriteLine("Add failed!");
+//}
+
+//List<Customer> customers = custCtrl.GetAll();
+
+//foreach (Customer eachCustomer in customers)
+//{
+//    Console.WriteLine($"{eachCustomer.Id} | {eachCustomer.Name} | {eachCustomer.City} | {eachCustomer.State} | {eachCustomer.Sales} | {eachCustomer.Active}");
+//}
+
+//bool success = custCtrl.DeleteRow(36);
+
+//Customer customerById = custCtrl.GetById(36);
+
+//if(customerById is null)
+//{
+//    Console.WriteLine("Not found!");
+//}
+//else
+//{
+//    Console.WriteLine($"{customerById.Id} | {customerById.Name} | {customerById.City}, {customerById.State} | {customerById.Sales} | {customerById.Active}");
+//}
+
+custCtrl.CloseConnection();
